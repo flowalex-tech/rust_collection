4185774335503f45603c9cb75a6d4312a0619063
flowalex-tech <github@mail.alexanderwolf.io>
2024-10-05 12:30:06 -0500

    renamed folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("This is a collection of simple rust Binaries, to run one, please do `cargo run --bin <binary_name>`");
+    println!("This is a collection of simple rust Binaries, to run one, please do `run --package rust_collection --example <binary_name>`");
 }
